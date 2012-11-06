5c39fcad9d11065149e29c84bd191712e6ad9d90
lyz <acr.user@gmail.com>
2012-11-06 16:50:26 +0900

    4th commit

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 var_dump($_SERVER);
+
+class test{
+	function test(){
+		$a = 100;
+		return $a;
+	}
+
+	function getA() {
+		$a = $this->test();
+		echo $a;
+	}
+}
+
+$test = new test;
+echo $test->getA();
