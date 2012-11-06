579bfd01bdf9eed3ee51bcf7e8f3da9c9574a88f
acruser <acr.user01@gmail.com>
2012-11-06 17:05:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
-var_dump($_SERVER);
-
 class test{
-	function test(){
+	function returnA(){
 		$a = 100;
 		return $a;
 	}
 
 	function getA() {
-		$a = $this->test();
+		$a = $this->returnA();
 		echo $a;
 	}
 }
 
 $test = new test;
-echo $test->getA();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<link rel="stylesheet" href="css/XXXX.css" media="all">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="js/XXXX.js"></script>
+</head>
+<body>
+<p><?php echo $test->getA(); ?></p>
+</body>
+</html>
